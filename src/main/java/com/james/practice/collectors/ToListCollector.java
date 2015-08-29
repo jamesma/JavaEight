b1da3a9c6b6e8313c24f3574561d47498ca7ecb6
James Ma <jamesma.1987@gmail.com>
2015-08-29 22:07:46 +0800

    Add practice for Collectors

---
+++ src/main/java/com/james/practice/collectors/ToListCollector.java
@@ -0,0 +1,48 @@
+package com.james.practice.collectors;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
+    @Override
+    public Supplier<List<T>> supplier() {
+        return ArrayList::new;
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return List::add;
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (list1, list2) -> {
+            list1.addAll(list2);
+            return list1;
+        };
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.unmodifiableSet(EnumSet.of(Characteristics.CONCURRENT,
+                                                      Characteristics.IDENTITY_FINISH));
+    }
+
+    // not recommended even if more compact and concise, as it doesn't promote reuse and characteristics cannot be
+    // passed
+    static <T> List<T> simple(Stream<T> stream) {
+        return stream.collect(ArrayList::new,
+                              List::add,
+                              List::addAll);
+    }
+}
--- src/main/java/com/james/practice/streams/dishes/DishPractice.java
+++ src/main/java/com/james/practice/streams/dishes/DishPractice.java
@@ -1,9 +1,9 @@
 package com.james.practice.streams.dishes;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.*;
 
 public class DishPractice {
 
@@ -51,6 +51,115 @@ public class DishPractice {
                         .collect(toList());         // List<String>
     }
 
+    long numberOfDishes() {
+        return Dish.menu.stream()
+                        .collect(counting()); // can be simplified to count()
+    }
+
+    Optional<Dish> findMaxCaloricDish() {
+        return Dish.menu.stream()
+                        .collect(maxBy(comparingInt(Dish::getCalories)));
+    }
+
+    Optional<Dish> findMaxCaloricDishUsingReduce() {
+        return Dish.menu.stream()
+                        .collect(reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2));
+    }
+
+    int totalCalories() {
+        return Dish.menu.stream()
+                        .collect(summingInt(Dish::getCalories));
+    }
+
+    int totalCaloriesUsingReduce() {
+        return Dish.menu.stream()
+                        .collect(reducing(0, Dish::getCalories, (i, j) -> i + j));
+    }
+
+    IntSummaryStatistics caloriesSummaryStatistics() {
+        return Dish.menu.stream()
+                        .collect(summarizingInt(Dish::getCalories));
+    }
+
+    String shortMenu() {
+        return Dish.menu.stream()
+                        .map(Dish::getName)
+                        .collect(joining());
+    }
+
+    String shortMenuDelimited() {
+        return Dish.menu.stream()
+                        .map(Dish::getName)
+                        .collect(joining(", "));
+    }
+
+    Map<Dish.Type, List<Dish>> dishesByType() {
+        return Dish.menu.stream()
+                        .collect(groupingBy(Dish::getType));
+    }
+
+    enum CaloricLevel {
+        DIET,
+        NORMAL,
+        FAT;
+
+        static CaloricLevel valueOf(Dish dish) {
+            int calories = dish.getCalories();
+            if (calories <= 400) {
+                return CaloricLevel.DIET;
+            } else if (calories <= 700) {
+                return CaloricLevel.NORMAL;
+            } else {
+                return CaloricLevel.FAT;
+            }
+        }
+    }
+
+    Map<CaloricLevel, List<Dish>> dishesByCaloricLevel() {
+        return Dish.menu.stream()
+                        .collect(groupingBy(CaloricLevel::valueOf));
+    }
+
+    Map<Dish.Type, Map<CaloricLevel, List<Dish>>> dishesByTypeCaloricLevel() {
+        return Dish.menu.stream()
+                        .collect(groupingBy(Dish::getType,
+                                            groupingBy(CaloricLevel::valueOf)));
+    }
+
+    Map<Dish.Type, Long> typesCount() {
+        return Dish.menu.stream()
+                        .collect(groupingBy(Dish::getType,
+                                            counting()));
+    }
+
+    Map<Dish.Type, Dish> mostCaloricByType() {
+        return Dish.menu.stream()
+                        .collect(groupingBy(Dish::getType,
+                                            collectingAndThen(maxBy(comparingInt(Dish::getCalories)),
+                                                              Optional::get)));
+    }
+
+    Map<Dish.Type, Set<CaloricLevel>> caloricLevelsByType() {
+        return Dish.menu.stream()
+                        .collect(groupingBy(Dish::getType,
+                                            mapping(CaloricLevel::valueOf, toSet())));
+    }
+
+    Map<Boolean, List<Dish>> partitionedMenu() {
+        return Dish.menu.stream()
+                        .collect(partitioningBy(Dish::isVegetarian));
+    }
+
+    List<Dish> vegetarianDishes() {
+        return partitionedMenu().get(true);
+    }
+
+    Map<Boolean, Map<Dish.Type, List<Dish>>> vegetarianDishesByType() {
+        return Dish.menu.stream()
+                        .collect(partitioningBy(Dish::isVegetarian,
+                                                groupingBy(Dish::getType)));
+    }
+
     public static void main(String[] args) {
         DishPractice dishPractice = new DishPractice();
 
@@ -68,5 +177,29 @@ public class DishPractice {
 
         System.out.println("\ndishNamesUniqueCharacters:");
         System.out.println(dishPractice.dishNamesUniqueCharacters());
+
+        System.out.println("\nshortMenu:");
+        System.out.println(dishPractice.shortMenu());
+
+        System.out.println("\nshortMenuDelimited:");
+        System.out.println(dishPractice.shortMenuDelimited());
+
+        System.out.println("\ndishesByType:");
+        System.out.println(dishPractice.dishesByType());
+
+        System.out.println("\ndishesByCaloricLevel:");
+        System.out.println(dishPractice.dishesByCaloricLevel());
+
+        System.out.println("\ndishesByTypeCaloricLevel:");
+        System.out.println(dishPractice.dishesByTypeCaloricLevel());
+
+        System.out.println("\ntypesCount:");
+        System.out.println(dishPractice.typesCount());
+
+        System.out.println("\nmostCaloricByType:");
+        System.out.println(dishPractice.mostCaloricByType());
+
+        System.out.println("\ncaloricLevelsByType:");
+        System.out.println(dishPractice.caloricLevelsByType());
     }
 }
